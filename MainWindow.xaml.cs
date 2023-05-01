134cccd3aa1137be21d23098d526ea5d26b0c79e
Hahaha12111 <nick92100760@gmail.com>
2023-05-01 15:44:29 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOne_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+            {
+                txtNumber.Text = "";
+            }
+
+            txtNumber.Text = txtNumber.Text + "1";
+            // txtNumber.Text += "1";  // 上面和下面的寫法意思是一樣的
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0") // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "2";
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "3";
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "4";
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "5";
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "6";
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "7";
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "8";
+        }
+
+        private void btnNine1_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + "9";
+
+        }
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = txtNumber.Text + "0";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
